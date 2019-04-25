d4fd505cf6126b1ac2c75049df7e806b90518d40
Dennis <dennissarmiento808@gmail.com>
2019-04-25 14:52:17 -0700

    up

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 
 var PORT = process.env.PORT || 4000;
 
-var databaseURL = "moviesblogdb";
+var databaseURL = process.env.MONGODB_URI || "moviesblogdb";
 var collections = ["scrapedMovies"];
 
 var db = mongojs(databaseURL, collections);
